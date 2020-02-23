fb023cd5b6f86c6a0df383e24de63583a42ac50d
雁回晴空 <zzlyw10@gmail.com>
2020-02-23 12:49:35 +0800

    change style of page management

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,4 @@
 
-var loc = window.location.href,
-    index = loc.indexOf('#');
-
-if (index > 0) {
-  window.location = loc.substring(0, index);
-}
 
 $.fn.exists = function () {
     return this.length > 0 ? this : false;
@@ -24,7 +18,6 @@ $.fn.exists = function () {
 	      checkSizeChange();
 	    }, 200);
 	}
-
 
 $(document).ready(function(){
 
@@ -187,137 +180,6 @@ $(document).ready(function(){
 
 	}
 	sidebar.init();
-
-	/*++++++++++++++++++++++++++++++++++++
-		pages 
-	++++++++++++++++++++++++++++++++++++++*/
-	var pager = {
-		pageContainer : $("div#main"),
-		pages : $("div.page"),
-		menuItems: $("ul#navigation"),
-		overlay : $("div#overlay"),
-		topz : "500",
-		init: function(){
-
-			self = this;
-			self.menuItems.on('click','li:not(.external)', function(e){
-				
-				e.preventDefault();
-				var $li = $(this),
-					$target = $($li.children('a').attr('href')),
-					currentPosition = $target.attr('data-pos'),
-					$secondary = self.pageContainer.children(".currentpage");
-				switch (currentPosition){
-					case "home" :
-						self.reset();
-						break;
-					case "p1" :
-						self.forward($target,$secondary);
-						break;
-					case "p3" :
-						if ( parseInt($target.attr('data-order')) === self.maxz() )
-						{	
-							// var $gotop2 = $target,
-							// 	$gotop1 = $secondary;
-							self.backward($target,$secondary);
-						} else {
-							self.forward($target,$secondary);
-						}
-					break;
-					default:
-						return false;
-				}
-			});
-
-			self.overlay.on('click',function(){
-				var $secondary = self.pageContainer.children(".currentpage");
-				var $target = self.pageContainer.children("[data-order="+self.maxz()+"]");
-				self.backward($target,$secondary);
-			});
-
-		},
-
-		reset : function (){
-
-			this.overlay.hide();
-			
-			var $gotop1 = this.pages.not(".home");
-			$gotop1.attr('data-pos','p1').removeAttr('data-order');
-			TweenLite.to($gotop1,0.4,{left:"100%",zIndex:0, onComplete:function(){
-				$gotop1.removeClass('currentpage');	
-			}});
-
-			this.hanndelMenu();
-		},
-
-		forward : function(gotop2 , /* optional */ gotop3){
-			
-			
-
-			self.hanndelMenu(gotop2);
-			self.overlay.show();
-			var maxz = self.maxz();
-			gotop2.addClass('currentpage');
-			gotop2.attr('data-pos','p2').removeAttr('data-order');
-			gotop3.attr('data-pos','p3').attr('data-order',maxz+1);
-			
-
-			( new TimelineLite() )
-				.set(gotop2,{ left:"100%",zIndex:self.topz})
-				.set(gotop3,{zIndex:maxz+1})
-				.to(gotop2,0.4,{left:"15%"})
-				.to(gotop3,0.3,{ left:0 , onComplete:function(){gotop3.removeClass('currentpage');} },"-=0.2");
-		},
-
-		backward : function (gotop2,gotop1){
-			
-
-			this.hanndelMenu(gotop2);
-			gotop2.exists() || this.overlay.hide();
-			gotop2.addClass('currentpage').removeAttr('data-order').attr('data-pos',"p2");
-			gotop1.attr('data-pos','p1');
-
-			(new TimelineLite())
-				.set(gotop2,{zIndex:self.topz-1})
-				.to(gotop2,0.4,{left:"15%"})
-				.to(gotop1,0.5,
-					{
-						left:"100%",
-						onComplete : function(){
-							gotop1.removeClass('currentpage');
-						}
-					},"-=0.3")
-				.set(gotop1,{zIndex:0});
-
-		},
-
-		maxz : function(){
-			
-			var levelArray = this.pages.map( function() {
-			    return $(this).attr('data-order');
-			}).get();
-			maxz = levelArray.length && Math.max.apply( Math, levelArray );
-			return maxz;
-		},
-
-		hanndelMenu : function(){
-			
-
-			var menuIndex = ( arguments.length ) ? ( (arguments[0].length) ? arguments[0].index() : 0 ):0;
-
-			this.menuItems.children().eq(menuIndex)
-				.addClass('currentmenu')
-				.siblings().removeClass('currentmenu');
-			
-
-		}
-	}
-
-
-
-	pager.reset();
-	pager.init();
-
 
 	/*++++++++++++++++++++++++++++++++++++
 		click event on ul.timeline titles
@@ -498,7 +360,7 @@ $(document).ready(function(){
 		removalDelay: 400,
 		mainClass: 'my-mfp-slide-bottom'
 	});
-	
+
 	/* Detect touch devices*/
 	function isTouchSupported(){
         //check if device supports touch
